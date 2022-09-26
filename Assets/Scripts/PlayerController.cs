29194c0ae9d033ce07541636808e0c0f0b442adf
Programming Bees <hasanakash799@gmail.com>
2022-09-27 01:06:09 +0600

    feta: player moveing controller added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float leftRightSpeed;
+    public float jumbSpeed;
+    float playerOriginalScaleX;
+    int playerFacingDirection = 1;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerOriginalScaleX = gameObject.transform.localScale.x;
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            playerFacingDirection = 1;
+            rb.velocity = new Vector2(leftRightSpeed * playerFacingDirection, rb.velocity.y);
+            gameObject.transform.localScale = new Vector3(playerOriginalScaleX, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            playerFacingDirection = -1;
+            rb.velocity = new Vector2(leftRightSpeed * playerFacingDirection, rb.velocity.y);
+            gameObject.transform.localScale = new Vector3(-playerOriginalScaleX, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumbSpeed);
+        }
+    }
+}
+//length is 32min
