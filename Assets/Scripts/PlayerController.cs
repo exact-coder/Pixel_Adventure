d00393f8d6fbfee0cdfa00899d8064277b542382
Programming Bees <hasanakash799@gmail.com>
2022-10-04 01:09:37 +0600

    chore: Loading Scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -136,6 +137,16 @@ public class PlayerController : MonoBehaviour
             //add sfx
             playerAudioSource.PlayOneShot(gameOverSFX,1f);
         }
+        if(collision.tag == "LebelOneComplete")
+        {
+            //SceneManager.LoadScene(1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
+    public void RetryButtonPressed()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
 }
-//vedio number10--1.5min
+//vedio number10--1.15min
