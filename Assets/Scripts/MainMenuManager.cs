3cf05e9d335a2168f84c8893a0d953605d315838
Programming Bees <hasanakash799@gmail.com>
2022-10-06 02:48:40 +0600

    Lebel 1 and 2 are polished and complete and some bug solving

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayButtonPressed()
+    {
+        DoTransition();
+
+        //wait for few second...
+        Invoke("LoadScene",1.5f);
+        
+
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitButtonPressed()
+    {
+        Application.Quit();
+    }
+
+    void DoTransition()
+    {
+        animator.SetTrigger("DoTransition");
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEditor.SceneManagement;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator TransitionAnimator;
 
     public Text pointText;
     public Text lifeText;
+    public Text totalPointText;
     public GameObject gameOverPanel;
 
     [SerializeField] GameObject bulletPrefab;
@@ -37,6 +35,27 @@ public class PlayerController : MonoBehaviour
     public AudioClip gameOverSFX;
     public AudioClip collectablesSFX;
 
+    bool isLeftButtonPressed;
+    bool isRightButtonPressed;
+    bool gameover = false;
+
+    public void LeftButtonPressed()
+    {
+        isLeftButtonPressed = true;
+    }
+    public void LeftButtonReleased()
+    {
+        isLeftButtonPressed = false;
+    }
+    public void RightButtonPressed()
+    {
+        isRightButtonPressed = true;
+    }
+    public void RightButtonReleased()
+    {
+        isRightButtonPressed = false;
+    }
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -55,7 +74,7 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        if (!gameover && Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) && !gameover || isRightButtonPressed &&!gameover)
         {
             playerFacingDirection = 1;
             rb.velocity = new Vector2(leftRightSpeed * playerFacingDirection, rb.velocity.y);
@@ -63,42 +82,52 @@ public class PlayerController : MonoBehaviour
 
             PlayAnimation("PlayerWalking", "NoAnimation");
         }
-        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        else if (!gameover && Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) && !gameover || isLeftButtonPressed && !gameover)
         {
             playerFacingDirection = -1;
             rb.velocity = new Vector2(leftRightSpeed * playerFacingDirection, rb.velocity.y);
             gameObject.transform.localScale = new Vector3(-playerOriginalScaleX, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
-            
+
             PlayAnimation("PlayerWalking", "NoAnimation");
         }
         else
         {
-            rb.velocity = new Vector2(0,rb.velocity.y);
-            PlayAnimation("PlayerIdle","NoAnimation");
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            PlayAnimation("PlayerIdle", "NoAnimation");
         }
         //Jump
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (!gameover && Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
         {
-            if(playerCollider.IsTouchingLayers(groundLayer))
-            {
-
-            rb.velocity = new Vector2(rb.velocity.x, jumbSpeed);
-                // add sfx
-                playerAudioSource.PlayOneShot(jumpSFX,1f);
-            }
+            JumpButtonPressed();
         }
-        if(Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return) && !gameover)
         {
-            //Generate Bullet
-            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-            //Add velocity to player facing directions...
-            bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(playerFacingDirection * bulletSpeed, 0f);
-            //add sfx
-            playerAudioSource.PlayOneShot(bulletSFX,1f);
-        } 
+            ShootButtonPressed();
+        }
     }
 
-    void PlayAnimation(string animationName1 , string animationName2)
+    public void JumpButtonPressed()
+    {
+        if (playerCollider.IsTouchingLayers(groundLayer) && !gameover)
+        {
+
+            rb.velocity = new Vector2(rb.velocity.x, jumbSpeed);
+            // add sfx
+            playerAudioSource.PlayOneShot(jumpSFX, 1f);
+        }
+    }
+
+    public void ShootButtonPressed()
+    {
+        //Generate Bullet
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        //Add velocity to player facing directions...
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(playerFacingDirection * bulletSpeed, 0f);
+        //add sfx
+        playerAudioSource.PlayOneShot(bulletSFX, 1f);
+    }
+
+    void PlayAnimation(string animationName1, string animationName2)
     {
         if (playerCollider.IsTouchingLayers(groundLayer))
         {
@@ -113,40 +142,65 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Collectable")
+        if (collision.tag == "Collectable")
         {
             point++;
             pointText.text = "Point : " + point;
             Destroy(collision.gameObject);
-            playerAudioSource.PlayOneShot(collectablesSFX,1f);
+            playerAudioSource.PlayOneShot(collectablesSFX, 1f);
         }
-        if(collision.tag == "Enemy")
+        if (collision.tag == "Enemy")
         {
             if (life == 0)
             {
                 gameOverPanel.SetActive(true);
+                gameover = true;
+                totalPointText.text = "Total Point : " + point;
             }
             else
             {
                 life--;
                 lifeText.text = "Life : " + life;
             }
-            
-            
+
+
 
             //add sfx
-            playerAudioSource.PlayOneShot(gameOverSFX,1f);
+            playerAudioSource.PlayOneShot(gameOverSFX, 1f);
         }
-        if(collision.tag == "LebelOneComplete")
+        if (collision.tag == "LebelComplete")
         {
-            //SceneManager.LoadScene(1);
+            TransitionAnimator.SetTrigger("DoTransition");
+            Invoke("LoadScene", 1.5f);
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        /*if (collision.tag == "LebelTwoComplete")
+        {
+            TransitionAnimator.SetTrigger("DoTransition");
+            SceneManager.LoadScene(0);
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }*/
+    }
+
+    void LoadScene()
+    {
+        //SceneManager.LoadScene(1);
+        int sceneNum = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(sceneNum);
+        if (sceneNum < 2)
+        {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
     }
+    
     public void RetryButtonPressed()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
 }
-//vedio number10--1.15min
+
